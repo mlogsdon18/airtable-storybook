d070a3e36fcdaa0080ce31b2e493b1f66b37aa2b
Megan Logsdon <megan@heysparkbox.com>
2022-03-29 09:11:23 -0400

    Updates to README

--- README.md
+++ README.md
@@ -7,10 +7,14 @@ To update the Airtable that is referenced, you can change the API key and base I
 ## Required
 
 In order for this to work, you will need to add a .env file.\
-You need to add the `AIRTABLE_API_KEY` variable set to your Airtable API key.
+The only necessary variable needed is `AIRTABLE_API_KEY` set to your Airtable API key.
 
 
 ## `npm run storybook`
 
 Runs the app in the development mode within Storybook.\
 Changes will be automatically updated in the browser.
+
+## AirtableList 
+This component grabs data from Airtable based on the arguments passed by the Story (baseId and tableName) and displays the "Name" and "Value" columns.
+
--- src/stories/AirtableList.jsx
+++ src/stories/AirtableList.jsx
@@ -6,20 +6,23 @@ export const AirtableList = ({ baseId, tableName }) => {
   const [records, setRecords] = useState([])
 
   useEffect(() => {
-
+    // Create access to the Airtable
     var base = new Airtable({endpointUrl: 'https://api.airtable.com', apiKey: process.env.AIRTABLE_API_KEY}).base(baseId);  
+    
+    // Select the specific table
+    // For this purpose we are just selecing the first page of records which can hold up to 100 records, we will need to refactor if more are needed
     base(tableName).select({
       view: "Grid view"
     }).firstPage(function (err, records) {
       if (err) { console.error(err); return; }
 
       setRecords(records);
-      console.log(records);
       return records;
     });
 
   }, [baseId, tableName])
 
+  // Use the record.get method with the column name to display the data
   return (
     <>
     {records.map((record) => (
